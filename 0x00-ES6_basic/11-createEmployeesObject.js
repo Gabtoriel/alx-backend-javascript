a9ff43f7fe66bc823439fe9dda5d2dfb2af6dade
Gabtoriel <73018647+Gabtoriel@users.noreply.github.com>
2024-03-21 00:54:22 +0100

    repaired lint errors

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,7 +1,7 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const employee_data = {
-    [`${departmentName}`]: [...employees,],
-  }
+  const employeeData = {
+    [`${departmentName}`]: [...employees],
+  };
 
-  return employee_data;
+  return employeeData;
 }
